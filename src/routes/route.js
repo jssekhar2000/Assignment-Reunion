d6702164220bcf4b85b340ba79073e1f3645f08b
jssekhar2000 <jssekhar2000@gmail.comm>
2022-09-08 16:41:53 +0530

    comments route

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,27 +2,27 @@ const router = require('express').Router();
 
 
 
-router.post('/api')
+router.post('/api/authenticate')    //should perform user authentication and return a JWT token.
 
-router.post('/api/follow/userId/:id')
+router.post('/api/follow/userId/:id')  //authenticated user would follow user with {id}
 
-router.post('/api/unfollow/userId/:id')
+router.post('/api/unfollow/userId/:id') // authenticated user would unfollow a user with {id}
 
-router.get('/api/user')
+router.get('/api/user')     // should authenticate the request and return the respective user profile.
 
-router.post('api/posts')
+router.post('api/posts')    // would add a new post created by the authenticated user.
 
-router.delete('api/posts/postId/:id')
+router.delete('api/posts/postId/:id') // would delete post with {id} created by the authenticated user.
 
-router.post('/api/like/postId/:id')
+router.post('/api/like/postId/:id')   //would like the post with {id} by the authenticated user.
 
-router.post('/api/unlike/postId/:id')
+router.post('/api/unlike/postId/:id')  // would unlike the post with {id} by the authenticated user.
 
-router.post('/api/comment/postId:id')
+router.post('/api/comment/postId:id')  // add comment for post with {id} by the authenticated user.
 
-router.get('api/posts/postId/:id')
+router.get('api/posts/postId/:id')  //would return a single post with {id} populated with its number of likes and comments  
 
-router.get('/api/all_posts')
+router.get('/api/all_posts')    // would return all posts created by authenticated user sorted by post time.
 
 
 
